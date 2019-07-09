64b0870ee3b430cf93d99089aaa462bd093410d3
normiaaa <normiaaa@gmail.com>
2019-07-09 23:01:05 +0300

    fix create account test

--- cypress/integration/automation/createAccount.spec.js
+++ cypress/integration/automation/createAccount.spec.js
@@ -1,30 +1,86 @@
 import { generateRandomEmail } from "../../utils";
+import newUser from "../../fixtures/newUserData.json";
 
-describe('Create Account', function() {
+describe("Create Account", function() {
     before(() => {
-        cy.visit('http://automationpractice.com/index.php');
+        cy.visit("http://automationpractice.com/index.php");
     });
 
-    it('Should correctly create a new account', () => {
-        cy.get('.login').click();
-        cy.get('#email_create').type(generateRandomEmail());
-        cy.get('#SubmitCreate').click();
-        cy.get('#id_gender2').check();
-        cy.get('#customer_firstname').type('N');
-        cy.get('#customer_lastname').type('Test');
-        cy.get('#passwd').type('testing');
-        cy.get('#days').select('10');
-        cy.get('#months').select('11');
-        cy.get('#years').select('1995');
-        cy.get('#newsletter').check();
-        cy.get('#optin').check();
-        cy.get('#company').type('Test');
-        cy.get('#address1').type('Testing');
-        cy.get('#city').type('Chicago');
-        cy.get('#id_state').select('Illinois');
-        cy.get('#postcode').type('00000');
-        cy.get('#phone').type('111111');
-        cy.get('#submitAccount').click();
-    });
+    it("Should correctly create a new account", () => {
+        const { USER_DATA } = newUser;
+        const {
+            id_country,
+            id_gender,
+            id_state,
+            customer_firstname,
+            customer_lastname,
+            passwd,
+            days,
+            months,
+            years,
+            firstname,
+            lastname,
+            company,
+            address1,
+            address2,
+            city,
+            postcode,
+            other,
+            phone,
+            phone_mobile,
+            alias,
+            dni,
+            email_create,
+            is_new_customer,
+            back,
+            submitAccount,
+        } = USER_DATA;
 
+        cy.request({
+            method: "POST",
+            form: true,
+            url: "http://automationpractice.com/index.php?controller=authentication",
+            body: {
+                id_gender,
+                customer_firstname,
+                customer_lastname,
+                email: generateRandomEmail(),
+                passwd,
+                days,
+                months,
+                years,
+                firstname,
+                lastname,
+                company,
+                address1,
+                address2,
+                city,
+                id_state,
+                postcode,
+                id_country,
+                other,
+                phone,
+                phone_mobile,
+                alias,
+                dni,
+                email_create,
+                is_new_customer,
+                back,
+                submitAccount,
+            },
+        });
+        cy.get(".login").click();
+        cy.get(".page-heading")
+            .invoke("text")
+            .should("equal", "My account");
+        cy.get(".info-account")
+            .invoke("text")
+            .should(
+                "equal",
+                "Welcome to your account. Here you can manage all of your personal information and orders."
+            );
+        cy.get(".account > span")
+            .invoke("text")
+            .should("equal", `${customer_firstname} ${customer_lastname}`);
+    });
 });
